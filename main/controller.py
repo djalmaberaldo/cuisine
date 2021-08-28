9198f9eab41c831b19acd1c8805dd4193ba78c0c
Djalma Beraldo Batista <djalmaberaldo.batista@gmail.com>
2021-08-28 20:50:22 -0300

    Splitting the methods

--- main/controller.py
+++ main/controller.py
@@ -16,6 +16,16 @@ current_pagination = 5
 
 def search_all(list_of_filters={}):
 
+    df = build_dataframe()
+    df = apply_filters(df, list_of_filters)
+    df = sort_dataframe(df)
+
+    df = df.head(current_pagination)
+    js = df.to_json(orient = 'records')
+    return  json.loads(js)
+
+
+def build_dataframe():
     restaurantes_df = pd.read_csv(file_to_search_restaurants, index_col=False, sep=",")
     cuisines_df = pd.read_csv(file_to_search_cuisines, index_col=False, sep=",")
 
@@ -24,24 +34,24 @@ def search_all(list_of_filters={}):
 
     print('Dropping columns..')
     df = df.drop(columns=["id", "cuisine_id"])
+    return df
 
+
+def apply_filters(data_frame, list_of_filters):
     if list_of_filters is not {}:
         for key, value in list_of_filters.items():
             if key in ['customer_rating']:
-                df = df.loc[df[key] >= int(value)]
+                data_frame = data_frame.loc[data_frame[key] >= int(value)]
             elif key in ['price', 'distance']:
-                df = df.loc[df[key] <= int(value)]
+                data_frame = data_frame.loc[data_frame[key] <= int(value)]
             else:
-                df = df.loc[df[key].str.contains(value)]
+                data_frame = data_frame.loc[data_frame[key].str.contains(value)]
+    return data_frame
+            
 
+def sort_dataframe(data_frame):
     print('Sorting ...')
-    df = df.sort_values(
+    return data_frame.sort_values(
         by=['distance','customer_rating', 'price', 'name_restaurant', 'name_cuisine'], 
         ascending=[True, False, True, True, True]
     )
-
-    df = df.head(current_pagination)
-    js = df.to_json(orient = 'records')
-    return  json.loads(js)
-
-
