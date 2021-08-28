3103f2f457f2b1213b9739eee5f92cf6551dda48
Djalma Beraldo Batista <djalmaberaldo.batista@gmail.com>
2021-08-28 19:13:06 -0300

    Start sorting and filtering improvement

--- main/controller.py
+++ main/controller.py
@@ -11,13 +11,18 @@ bp = Blueprint('controller', __name__)
 package_dir = os.path.dirname(os.path.realpath(__file__+'../'))
 file_to_search_restaurants =  os.path.join(package_dir,'../restaurants.csv')
 
+current_pagination = 5
+
 def search_all(restaurant_name=None):
 
     df = pd.read_csv(file_to_search_restaurants,  index_col=False, sep=",")
 
     if restaurant_name is not None:
         print('Finding by name...')
-        df = df.loc[df['name'] == restaurant_name]
+        df = df.loc[df['name'].str.contains(restaurant_name)]
+
+    print('Sorting ...')
+    df = df.sort_values(by=['distance','customer_rating'])
 
     js = df.to_json(orient = 'records')
     return js
