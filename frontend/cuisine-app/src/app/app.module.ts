591f202dba1cdfbeee985bc05b604039df055fe3
Djalma Beraldo Batista <djalmaberaldo.batista@gmail.com>
2021-08-29 12:55:41 -0300

    Generated frontend app

---
+++ frontend/cuisine-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BestMatchModule } from './best-match/best-match.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BestMatchModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
