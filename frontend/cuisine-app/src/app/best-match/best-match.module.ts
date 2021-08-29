fa9b8562b3a67460b01258418689088915b1c5cd
Djalma Beraldo Batista <djalmaberaldo.batista@gmail.com>
2021-08-29 17:20:12 -0300

    Changed search fields

--- frontend/cuisine-app/src/app/best-match/best-match.module.ts
+++ frontend/cuisine-app/src/app/best-match/best-match.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BestMatchRoutingModule } from './best-match.route';
 import { SearchPageComponent } from './search-page/search-page.component';
 
@@ -8,7 +9,9 @@ import { SearchPageComponent } from './search-page/search-page.component';
   declarations: [SearchPageComponent],
   imports: [
     CommonModule,
+    FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
     BestMatchRoutingModule
   ],
   exports: [SearchPageComponent]
--- frontend/cuisine-app/src/app/best-match/search-page/search-page.component.ts
+++ frontend/cuisine-app/src/app/best-match/search-page/search-page.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Restaurant } from './../service/restaurant.model';
 import { RestaurantService } from './../service/restaurants.service.component';
 
@@ -11,7 +12,6 @@ import { RestaurantService } from './../service/restaurants.service.component';
 export class SearchPageComponent implements OnInit {
 
   restaurants: Restaurant[] = [];
-  searchRef: string = '';
   constructor(private restaurantService: RestaurantService) { }
 
   ngOnInit(): void {
@@ -21,10 +21,8 @@ export class SearchPageComponent implements OnInit {
   searchRestaurants() {
     this.restaurantService
       .query({
-        "customer_rating": 3,
-        "price": 10
-      })
-      .subscribe(
+
+      }).subscribe(
         (body: Restaurant[]) => {
           this.restaurants = body;
         },
