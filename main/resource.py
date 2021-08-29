143086534d971cabc9fc6c1e9702112bb2e246a0
Djalma Beraldo Batista <djalmaberaldo.batista@gmail.com>
2021-08-28 21:44:29 -0300

    Addind error handler to route

--- main/resource.py
+++ main/resource.py
@@ -1,4 +1,3 @@
-
 import json
 import logging
 
@@ -13,23 +12,22 @@ expect_keys = ['distance','customer_rating', 'price', 'name_restaurant', 'name_c
 def search():
     list_of_filters = request.args.to_dict()
 
-    keys = list_of_filters.keys()
-
-    print('Validating parameters...')
-    for k in keys:
-        if k not in expect_keys:
-            return {
-                "message": "Invalid Parameters",
-                "status": 403
-            }
-
     results = controller.search_all(list_of_filters)
     return {
         "status": 200,
         "body": json.loads(results)
     } 
-        
+
+
 @bp.errorhandler(500)
 def internal_server_error():
-    return render_template('500.html'), 500
-    
+    return render_template('500.html'), 
+
+
+@bp.before_request
+def validate_keys():
+    keys = request.args.to_dict().keys()
+    print('Validating parameters...')
+    for k in keys:
+        if k not in expect_keys:
+            return 'Invalid Parameters', 400
